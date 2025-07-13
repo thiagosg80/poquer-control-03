c93334361a309fe3e284a77cf6f2fd6b5218449c
Thiago Goncalves <thiagosg80@gmail.com>
2025-07-13 20:50:04 -0300

    Initial commit

---
+++ src/main/java/com/app/poquer_control_03/PoquerControl03Application.java
@@ -0,0 +1,13 @@
+package com.app.poquer_control_03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoquerControl03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoquerControl03Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/poquer_control_03/PoquerControl03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.poquer_control_03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoquerControl03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
